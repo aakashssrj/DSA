a8b9046a294be4c929de6d23d38ca7e9cfd56a87
Aakashssrj <aakashsah591@gmail.com>
2025-03-08 23:06:10 +0530

    Full Stack

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import LoginRegister from '../src/pages/LoginRegister/LoginRegister'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LoginRegister/>
+    
+    </>
+  )
+}
+
+
+export default App
